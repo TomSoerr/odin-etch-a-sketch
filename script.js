bff5fdcd2f284e23462c469efc1fb195c2eae504
TomSoerr <tomix2004@gmail.com>
2022-03-30 16:24:49 +0200

    Add changeable background and pen color

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const canvas = document.querySelector('.container-canvas');
 const toolbar = document.querySelector('.container-toolbar');
+const background = document.querySelector('#background');
+const backgroundColor = document.querySelector('#background-color');
 const pen = document.querySelector('#pen');
-const borderColor = 'rgb(127, 127, 127)';
-const backgroundColor = 'white';
-const canvasGrid = false;
-const currentColor = 'grey';
+const penColor = document.querySelector('#pen-color');
+
+let borderColor = 'rgb(127, 127, 127)';
+let canvasGrid = false;
+let currentGridSize = 16;
+let currentPenColor = 'black';
+
+let popupPen = false;
+let popupBackground = false;
+
+let selected = 'pen';
 
 
 function buildCanvasGrid(size) {
@@ -12,7 +21,6 @@ function buildCanvasGrid(size) {
     while (canvas.firstChild) {
         canvas.removeChild(canvas.lastChild);
     }
-    canvas.style.backgroundColor = backgroundColor;
     const row = size;
     const col = size;
 
@@ -45,22 +53,68 @@ function buildCanvasGrid(size) {
     }
 }
 
-function selectPen() {
-    pen.style.webkitFilter = 'opacity(100%)';
-    pen.style.filter = 'opacity(100%)';
-    pen.style.borderLeft = 'white solid 0.5vh';
 
+function popup(which) {
+    switch (which) {
+        case 'pen':
+            if (!popupPen) {
+                handler = function(event){
+                    currentPenColor = event.target.value;
+                    event.stopPropagation();
+                    penColor.style.visibility = 'hidden';
+                    penColor.removeEventListener('change', handler);
+                    popupPen = false;
+                };
+                popupPen = true;
+                penColor.style.visibility = 'visible';
+                penColor.addEventListener('change', handler);
+            } else {
+                penColor.style.visibility = 'hidden';
+                penColor.removeEventListener('change', handler);
+                popupPen = false;
+            }
+            break;
+
+        case 'background':
+            if (!popupBackground) {
+                handler = function(event){
+                    canvas.style.backgroundColor = event.target.value;
+                    event.stopPropagation();
+                    backgroundColor.style.visibility = 'hidden';
+                    backgroundColor.removeEventListener('change', handler);
+                    popupBackground = false;
+                    background.classList.remove('active');
+                };
+                popupBackground = true;
+                backgroundColor.style.visibility = 'visible';
+                background.classList.add('active');
+                backgroundColor.addEventListener('change', handler);
+            } else {
+                backgroundColor.style.visibility = 'hidden';
+                backgroundColor.removeEventListener('change', handler);
+                popupBackground = false;
+                background.classList.remove('active');
+            }
+            break;
+            
+    }
 }
 
 
 function checkMode(event) {
-    event.target.style.backgroundColor = currentColor;
+    switch (selected) {
+        case 'pen':
+            event.target.style.backgroundColor = currentPenColor;
+            break;
+    }
 }
-
 function load() {
-    buildCanvasGrid(64);
-    
-    selectPen();
+    canvas.style.backgroundColor = 'white';
+
+    buildCanvasGrid(currentGridSize);
+
+    selected = 'pen';
+    pen.classList.add('active');
 
     let mouseDown = false;
     canvas.addEventListener('mousedown', (event) => {
@@ -77,14 +131,23 @@ function load() {
         }
         event.stopPropagation();
     });
+   canvas.addEventListener('click', (event) => {
+        if (event.target.classList.contains('pixel')) {
+            checkMode(event);
+        }
+        event.stopPropagation();
+    });
 
     toolbar.addEventListener('click', (event) => {
         if (event.target.id === 'pen') {
-            selectPen();
+            popup('pen');
+            selected = 'pen';
+            pen.classList.add('active');
+        } else if (event.target.id === 'background') {
+            popup('background');
         }
+        event.stopPropagation();
     });
-    
-
 }
 
 load();
