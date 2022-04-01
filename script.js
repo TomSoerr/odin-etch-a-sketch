9dc16120eeefeeabdf6ff808a622a503ecdb9d35
TomSoerr <tomix2004@gmail.com>
2022-04-01 12:43:53 +0200

    Make script.js code clean and readable

--- script.js
+++ script.js
@@ -28,8 +28,10 @@ let pencil = 'pen';
 penBtn.classList.add('active');
 //
 
-// 6 as a last one build the bucket tool 
-// 7 maybe add grid size popup
+// todo:
+// bucket tool
+// grid size popup
+
 
 const buildCanvas = function buildCanvasGrid() {
     while (canvas.firstChild) {
@@ -38,6 +40,7 @@ const buildCanvas = function buildCanvasGrid() {
 
     const row = currentGridSize;
     const col = currentGridSize;
+
     if (gridLines) {
         canvas.style.borderTop = `${borderColor} thin solid`;
         canvas.style.borderLeft = `${borderColor} thin solid`;
@@ -65,7 +68,7 @@ const buildCanvas = function buildCanvasGrid() {
         }
         canvas.appendChild(rowDiv);
     }
-}
+};
 
 
 const clearCanvas = function clearCanvasWithAnimation() {
@@ -76,9 +79,8 @@ const clearCanvas = function clearCanvasWithAnimation() {
             duration: 500,
             easing: 'ease-out'
         });
-
     setTimeout(buildCanvas, 500);
-}
+};
 
 
 const promptNewSize = function promptForNewCanvasSize() {
@@ -91,45 +93,6 @@ const promptNewSize = function promptForNewCanvasSize() {
     } else {
         promptNewSize();
     }
-}
-
-
-const rgb = function getHtmlRgbValues(value, index) {
-    switch (index) {
-        case 'r':
-            return parseInt(value.match(/\d+/g)[0]);
-        case 'g':
-            return parseInt(value.match(/\d+/g)[1]);
-        case 'b':
-            return parseInt(value.match(/\d+/g)[2]);
-    }
-}
-const rgbToInt = rgb
-
-
-const changeShade = function changeShadeWithBackground(item) {
-    const shade = item.dataset.shade
-    const color = canvasBackground.style.backgroundColor;
-    let r = rgb(color, 'r');
-    let g = rgb(color, 'g');
-    let b = rgb(color, 'b');
-    const cW = function calculateWhiteShade(color) {
-        return color + Math.round((255 - color) / 10 * shade);
-    }
-    const cB = function calculateBlackShade(color) {
-        return color - Math.round(color / 10 * -shade);
-    }
-    switch (true) {
-        case shade == 0:
-            item.style.backgroundColor = color;
-            break;
-        case shade > 0:
-            item.style.backgroundColor = `rgb(${cW(r)}, ${cW(g)}, ${cW(b)})`;
-            break;
-        case shade < 0:
-            item.style.backgroundColor = `rgb(${cB(r)}, ${cB(g)}, ${cB(b)})`;
-            break;
-    };
 };
 
 
@@ -152,15 +115,117 @@ const toggleGrid = function toggleGridLines() {
             item.style.borderBottom = 'none';
         });
     }
-}
+};
 
 
-function paint(event) {
+const bucketTool = function bucketTool(event) {
+};
+
+
+const rgb = function getHtmlRgbValues(value, index) {
+    switch (index) {
+        case 'r':
+            return parseInt(value.match(/\d+/g)[0]);
+        case 'g':
+            return parseInt(value.match(/\d+/g)[1]);
+        case 'b':
+            return parseInt(value.match(/\d+/g)[2]);
+    }
+};
+
+
+const changeShade = function changeShadeWithBackground(item) {
+    const shade = item.dataset.shade
+    const style = item.style;
+    const bgColor = canvasBackground.style.backgroundColor;
+    let r = rgb(bgColor, 'r');
+    let g = rgb(bgColor, 'g');
+    let b = rgb(bgColor, 'b');
+
+    const cW = function calculateWhiteShade(color) {
+        return color + Math.round((255 - color) / 10 * shade);
+    }
+
+    const cB = function calculateBlackShade(color) {
+        return color - Math.round(color / 10 * -shade);
+    }
+
+    switch (true) {
+        case shade == 0:
+            style.backgroundColor = bgColor;
+            break;
+
+        case shade > 0:
+            style.backgroundColor = `rgb(${cW(r)}, ${cW(g)}, ${cW(b)})`;
+            break;
+
+        case shade < 0:
+            style.backgroundColor = `rgb(${cB(r)}, ${cB(g)}, ${cB(b)})`;
+            break;
+    };
+};
+
+
+const addWhiteOrBlack = function addWhiteOrBlackShading(event) {
+    const shade = event.target.dataset.shade;
+    const style = event.target.style
+    const bgColor = canvasBackground.style.backgroundColor;
+    const penColor = event.target.dataset.color;
+
+    const cW = function calculateWhiteShade(color) {
+        return color + Math.round((255 - color) / 10 * shade);
+    }
+
+    const cB = function calculateBlackShade(color) {
+        return color - Math.round(color / 10 * -shade);
+    }
+
+    if (penColor == 'bg') {
+        const r = rgb(bgColor, 'r');
+        const g = rgb(bgColor, 'g');
+        const b = rgb(bgColor, 'b');
+        switch (true) {
+            case shade == 0:
+                style.backgroundColor = bgColor;
+                break;
+
+            case shade > 0:
+                style.backgroundColor = `rgb(${cW(r)}, ${cW(g)}, ${cW(b)})`;
+                break;
+
+            case shade < 0:
+                style.backgroundColor = `rgb(${cB(r)}, ${cB(g)}, ${cB(b)})`;
+                break;
+        };
+    } else {
+        const r = rgb(penColor, 'r');
+        const g = rgb(penColor, 'g');
+        const b = rgb(penColor, 'b');
+        switch (true) {
+            case shade == 0:
+                style.backgroundColor = penColor;
+                break;
+
+            case shade > 0:
+                style.backgroundColor = `rgb(${cW(r)}, ${cW(g)}, ${cW(b)})`;
+                break;
+
+            case shade < 0:
+                style.backgroundColor = `rgb(${cB(r)}, ${cB(g)}, ${cB(b)})`;
+                break;
+        };
+    }
+};
+
+
+const paint = function paintOnTheCanvas(event) {
+    const style = event.target.style;
+    const dataset = event.target.dataset;
     switch (pencil) {
         case 'pen':
-            event.target.style.backgroundColor = currentPenColor;
-            event.target.dataset.color = event.target.style.backgroundColor;
-            event.target.dataset.shade = 0;
+            style.backgroundColor = currentPenColor;
+            dataset.color = style.backgroundColor;
+            dataset.shade = 0;
             break;
 
         case 'bucket':
@@ -168,110 +233,36 @@ function paint(event) {
 
         case 'random':
             rand = () => Math.floor(Math.random() * 256);
-            event.target.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`;
-            event.target.dataset.color = event.target.style.backgroundColor;
-            event.target.dataset.shade = 0;
+            style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`;
+            dataset.color = style.backgroundColor;
+            dataset.shade = 0;
             break;
 
         case 'eraser':
-            event.target.style.backgroundColor = 'transparent';
-            event.target.dataset.color = 'transparent';
-            event.target.dataset.shade = 0;
+            style.backgroundColor = 'transparent';
+            dataset.color = 'transparent';
+            dataset.shade = 0;
             break;
 
         case 'add-white':
-            if (event.target.dataset.shade >= 10) return;
-            if (event.target.dataset.color === 'transparent') {
-                event.target.dataset.color = 'bg';
+            if (dataset.shade >= 10) return;
+            if (dataset.color === 'transparent') {
+                dataset.color = 'bg';
             }
-            event.target.dataset.shade++;
+            dataset.shade++;
             addWhiteOrBlack(event);
             break;
 
         case 'add-black':
-            if (event.target.dataset.shade <= -10) return;
-            if (event.target.dataset.color === 'transparent') {
-                event.target.dataset.color = 'bg';
+            if (dataset.shade <= -10) return;
+            if (dataset.color === 'transparent') {
+                dataset.color = 'bg';
             }
-            event.target.dataset.shade--;
+            dataset.shade--;
             addWhiteOrBlack(event);
             break;
         }
-    }
-
-function addWhiteOrBlack(event) {
-    function rgbToInt(value, index) {
-        switch (index) {
-            case 'r':
-                return parseInt(value.match(/\d+/g)[0]);
-            case 'g':
-                return parseInt(value.match(/\d+/g)[1]);
-            case 'b':
-                return parseInt(value.match(/\d+/g)[2]);
-        }
-    }
-    const setShade = function(event) {
-
-    }
-    if (event.target.dataset.shade == 0) {
-        
-        if (event.target.dataset.color === 'bg') {
-            event.target.style.backgroundColor = canvasBackground.style.backgroundColor;
-        } else {
-            event.target.style.backgroundColor = canvasBackground.style.backgroundColor;
-        }
-        event.target.style.backgroundColor = event.target.dataset.color;
-    } else if (event.target.dataset.shade > 0) {
-        if (event.target.dataset.color === 'bg') {
-            const rOriginal = rgbToInt(canvasBackground.style.backgroundColor, 'r');
-            const rDifference = Math.round((255 - rOriginal) / 10 * event.target.dataset.shade);
-            const r = rOriginal + rDifference;
-            const gOriginal = rgbToInt(canvasBackground.style.backgroundColor, 'g');
-            const gDifference = Math.round((255 - gOriginal) / 10 * event.target.dataset.shade);
-            const g = gOriginal + gDifference;
-            const bOriginal = rgbToInt(canvasBackground.style.backgroundColor, 'b');
-            const bDifference = Math.round((255 - bOriginal) / 10 * event.target.dataset.shade);
-            const b = bOriginal + bDifference;
-            event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        } else {
-            const rOriginal = rgbToInt(event.target.dataset.color, 'r');
-            const rDifference = Math.round((255 - rOriginal) / 10 * event.target.dataset.shade);
-            const r = rOriginal + rDifference;
-            const gOriginal = rgbToInt(event.target.dataset.color, 'g');
-            const gDifference = Math.round((255 - gOriginal) / 10 * event.target.dataset.shade);
-            const g = gOriginal + gDifference;
-            const bOriginal = rgbToInt(event.target.dataset.color, 'b');
-            const bDifference = Math.round((255 - bOriginal) / 10 * event.target.dataset.shade);
-            const b = bOriginal + bDifference;
-            event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        }
-    } else if (event.target.dataset.shade < 0) {
-
-        if (event.target.dataset.color === 'bg') {
-            const rOriginal = rgbToInt(canvasBackground.style.backgroundColor, 'r');
-            const rDifference = Math.round(rOriginal / 10 * -event.target.dataset.shade);
-            const r = rOriginal - rDifference;
-            const gOriginal = rgbToInt(canvasBackground.style.backgroundColor, 'g');
-            const gDifference = Math.round(gOriginal / 10 * -event.target.dataset.shade);
-            const g = gOriginal - gDifference;
-            const bOriginal = rgbToInt(canvasBackground.style.backgroundColor, 'b');
-            const bDifference = Math.round(bOriginal / 10 * -event.target.dataset.shade);
-            const b = bOriginal - bDifference;
-            event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        } else {
-            const rOriginal = rgbToInt(event.target.dataset.color, 'r');
-            const rDifference = Math.round(rOriginal / 10 * -event.target.dataset.shade);
-            const r = rOriginal - rDifference;
-            const gOriginal = rgbToInt(event.target.dataset.color, 'g');
-            const gDifference = Math.round(gOriginal / 10 * -event.target.dataset.shade);
-            const g = gOriginal - gDifference;
-            const bOriginal = rgbToInt(event.target.dataset.color, 'b');
-            const bDifference = Math.round(bOriginal / 10 * -event.target.dataset.shade);
-            const b = bOriginal - bDifference;
-            event.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        }
-    }
-}
+};
 
 
 const addCanvasEventListeners = function() {
@@ -296,14 +287,14 @@ const addCanvasEventListeners = function() {
         }
         event.stopPropagation();
     });
-}
+};
 
 
 const deactivateAll = function deactivateAllActiveClasses() {
     toolbar.querySelectorAll('.only-one-active').forEach(item => {
         item.classList.remove('active');
     });
-}
+};
 
 
 const showPopup = function showColorPickerPopup(which) {
@@ -352,11 +343,10 @@ const showPopup = function showColorPickerPopup(which) {
             }
             break;    
     }
-}
+};
 
 
 const addToolbarEventListeners = function() {
-
     toolbar.addEventListener('click', (event) => {
         event.stopPropagation();   
         switch (event.target.id) {
@@ -414,7 +404,7 @@ const addToolbarEventListeners = function() {
                 break;
         }
     });
-}
+};
 
 
 addCanvasEventListeners();
